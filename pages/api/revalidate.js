f4c763dbe7c9ae37c8ecb1cabe2340ca2ee863f6
Jon Meyers <jonathon.d.meyers@gmail.com>
2022-06-30 15:12:10 +1000

    fix path for revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,7 +1,11 @@
 const handler = async (req, res) => {
-  const pathToRevalidate = req.body?.record?.id || req.body?.old_record?.id;
   await res.revalidate("/");
+
+  const pathToRevalidate = `/${
+    req.body?.record?.id || req.body?.old_record?.id
+  }`;
   await res.revalidate(pathToRevalidate);
+
   return res.send({ revalidated: true, paths: ["/", pathToRevalidate] });
 };
 
